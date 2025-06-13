7555dd6ed9991c7147e1cedc1159aef8d907fabd
Amolkharade1 <nonamesry069@gmail.com>
2025-06-13 16:54:52 +0530

    index.js

--- Final HRMS 0.7/hrms-dashboard/src/index.js
+++ Final HRMS 0.7/hrms-dashboard/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from "./axios"; // yeh tu likhega
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
